73d132c1bb65517f71f29526715b5e8ebe0a1986
Danny Banks <djb@amazon.com>
2016-11-28 11:18:21 -0800

    Cleaning up the bin

--- build.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-var StyleDictionary = require('style-dictionary');
-
-var pwd = process.cwd();
-var config = JSON.parse( fs.readFileSync(pwd + '/config.json') );
-var sd = StyleDictionary.extend( config );
-
-sd.buildAllPlatforms();
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
+  VERSION: '1.0.0',
   properties: {},
   allProperties: [],
   defaultBuildPath: 'build/',
