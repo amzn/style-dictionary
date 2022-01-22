6c0d29ff50cccf9d2d5b60951f51741e4af865d8
Danny Banks <djb@amazon.com>
2022-01-21 16:29:12 -0800

    feat(references): adding configurable reference syntax

---
+++ __integration__/customReferenceCharacters.test.js
@@ -0,0 +1,33 @@
+const fs = require('fs-extra');
+const StyleDictionary = require('../index');
+const {buildPath} = require('./_constants');
+
+describe('integration', () => {
+  describe('custom reference characters', () => {
+    StyleDictionary.extend({
+      tokens: {
+        color: {
+          red: { value: '#f00' },
+          error: { value: '🐈color/red/value🐈‍⬛' }
+        }
+      },
+      separator: '/',
+      opening_character: '🐈',
+      closing_character: '🐈‍⬛',
+      platforms: {
+        css: {
+          transformGroup: 'css',
+          buildPath,
+          files: [{
+            destination: 'variables.css',
+            format: 'css/variables'
+          }]
+        }
+      }
+    }).buildAllPlatforms();
+    const output = fs.readFileSync(`${buildPath}variables.css`, {encoding:'UTF-8'});
+    it(`should match snapshot`, () => {
+      expect(output).toMatchSnapshot();
+    });
+  });
+});
