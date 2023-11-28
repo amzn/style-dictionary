b7fdbac5250f9122a4a671f9aeb1b5aab2d62efc
jorenbroekema <joren.broekema@gmail.com>
2023-11-28 12:04:52 +0100

    chore: fix inject-version script

--- scripts/inject-version.js
+++ scripts/inject-version.js
@@ -1,6 +1,7 @@
 import fs from 'node:fs';
 
-const indexContent = fs.readFileSync('index.js', 'utf-8');
+const filePath = 'lib/StyleDictionary.js';
+const indexContent = fs.readFileSync(filePath, 'utf-8');
 const { version } = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
 const newIndexContent = indexContent.replace('<? version placeholder ?>', version);
-fs.writeFileSync('index.js', newIndexContent, 'utf-8');
+fs.writeFileSync(filePath, newIndexContent, 'utf-8');
