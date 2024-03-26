daa78e17ca021bfb752743a8393dee6f123494eb
Lukas Oppermann <lukasoppermann@github.com>
2024-03-26 23:49:09 +0100

    fix(types): added missing type exports
    * Adding all types to index.d.ts
    
    Added all types to index.d.ts to be used by others, fixes #1135
    
    * Update types/index.d.ts
    
    Co-authored-by: Joren Broekema <joren.broekema@gmail.com>
    
    * fix(types): added missing type exports
    
    ---------
    
    Co-authored-by: Joren Broekema <joren.broekema@gmail.com>

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,21 +1,22 @@
 export type { Action } from './Action.d.ts';
 
-export type { PlatformConfig, Config } from './Config.d.ts';
+export { PlatformConfig, Config, LocalOptions, LogConfig } from './Config.d.ts';
 
 export type {
   DesignToken,
   DesignTokens,
   TransformedToken,
   TransformedTokens,
+  Dictionary
 } from './DesignToken.d.ts';
 
-export type { FileHeader, File } from './File.d.ts';
+export type { FileHeader, File, FormattingOptions } from './File.d.ts';
 
-export type { Filter } from './Filter.d.ts';
+export type { Filter, Matcher } from './Filter.d.ts';
 
-export type { Format } from './Format.d.ts';
+export type { Format, FormatterArguments, Formatter } from './Format.d.ts';
 
-export type { Parser } from './Parser.d.ts';
+export type { Parser, ParserOptions } from './Parser.d.ts';
 
 export type { Preprocessor } from './Preprocessor.d.ts';
 
@@ -25,3 +26,5 @@ export type {
   AttributeTransform,
   ValueTransform,
 } from './Transform.d.ts';
+
+export type { Volume } from './Volume.d.ts';
