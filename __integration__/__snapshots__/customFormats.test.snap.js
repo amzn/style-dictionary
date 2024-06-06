256fbf30a79e0c6c44a0034e46547ea2c635a5a4
jorenbroekema <joren.broekema@gmail.com>
2024-06-06 12:32:18 +0200

    fix: make css stroke, cubicbezier and fontfamily transitive

--- __integration__/__snapshots__/customFormats.test.snap.js
+++ __integration__/__snapshots__/customFormats.test.snap.js
@@ -838,10 +838,12 @@ snapshots["integration custom formats inline custom with new args should match s
           "type": "value"
         },
         "fontFamily/css": {
-          "type": "value"
+          "type": "value",
+          "transitive": true
         },
         "cubicBezier/css": {
-          "type": "value"
+          "type": "value",
+          "transitive": true
         },
         "strokeStyle/css/shorthand": {
           "type": "value",
@@ -1790,10 +1792,12 @@ snapshots["integration custom formats register custom format with new args shoul
           "type": "value"
         },
         "fontFamily/css": {
-          "type": "value"
+          "type": "value",
+          "transitive": true
         },
         "cubicBezier/css": {
-          "type": "value"
+          "type": "value",
+          "transitive": true
         },
         "strokeStyle/css/shorthand": {
           "type": "value",
