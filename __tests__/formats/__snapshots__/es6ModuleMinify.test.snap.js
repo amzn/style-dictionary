b80cb0714a90172213ba3f3bed2874e572efb242
Jack Holden <75576058+jackholden@users.noreply.github.com>
2024-11-03 17:20:15 +0000

    feat(format): additional es6 module object test and snapshot

---
+++ __tests__/formats/__snapshots__/es6ModuleMinify.test.snap.js
@@ -0,0 +1,16 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["formats javascript/es6-module should be a valid JS file and match snapshot"] = 
+`/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+export default {
+  "color": {
+    "red": "#FF0000"
+  }
+};
+`;
+/* end snapshot formats javascript/es6-module should be a valid JS file and match snapshot */
+
