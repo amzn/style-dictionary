9ecbda977c8fb9e387c81493427787b31d7c80c7
JC Franco <jfranco@esri.com>
2025-03-24 14:58:15 -0700

    test: remove unrelated test snapshot from initial approach

--- __tests__/formats/__snapshots__/typeScriptEs6Declarations.test.snap.js
+++ __tests__/formats/__snapshots__/typeScriptEs6Declarations.test.snap.js
@@ -49,14 +49,3 @@ export const fontFamily: string;
 `;
 /* end snapshot formats typescript/es6-declarations with DTCG tokens and without outputStringLiterals should match snapshot */
 
-snapshots["formats typescript/es6-declarations with formatted comment should match snapshot"] = 
-`/**
- * Do not edit directly, this file was auto-generated.
- */
-
-export const colorBlue: string; /** Used for errors */
-export const colorRed: string;
-export const fontFamily: string;
-`;
-/* end snapshot formats typescript/es6-declarations with formatted comment should match snapshot */
-
