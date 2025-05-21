d9f3247585d6a8ad1e0ecd2cd7d59d185e734f13
jorenbroekema <joren.broekema@gmail.com>
2025-05-21 13:49:51 +0200

    chore: delete copyright headers in test files

--- foo.js
+++ foo.js
@@ -1,12 +1,12 @@
 import { globSync } from 'glob';
 import fs from 'node:fs/promises';
 
-const files = globSync('**/*', { ignore: 'node_modules/**/*', nodir: true });
+const files = globSync('**/*.test.js', { ignore: 'node_modules/**/*', nodir: true });
 
 const deleteCopyrights = async (filePath) => {
   const contents = await fs.readFile(filePath, 'utf-8');
-  const copyMatch = contents.match(/^(.|\n)*Copyright 2017 Amazon(.|\n)*?((\/|\*)\/\n)/gm);
-  if (copyMatch[0]) {
+  const copyMatch = contents.match(/^(.|\n)*Copyright(.|\n)*?((\/|\*)\/\n)/gm);
+  if (copyMatch?.[0]) {
     const newContents = contents.split(copyMatch)[1].trimStart();
     await fs.writeFile(filePath, newContents, 'utf-8');
   }
