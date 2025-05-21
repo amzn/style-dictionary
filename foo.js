9ddac9c8d256d536ad810928be2cc84970c61316
jorenbroekema <joren.broekema@gmail.com>
2025-05-21 13:38:58 +0200

    chore: update pkg json license and copyright

---
+++ foo.js
@@ -0,0 +1,15 @@
+import { globSync } from 'glob';
+import fs from 'node:fs/promises';
+
+const files = globSync('**/*', { ignore: 'node_modules/**/*', nodir: true });
+
+const deleteCopyrights = async (filePath) => {
+  const contents = await fs.readFile(filePath, 'utf-8');
+  const copyMatch = contents.match(/^(.|\n)*Copyright 2017 Amazon(.|\n)*?((\/|\*)\/\n)/gm);
+  if (copyMatch[0]) {
+    const newContents = contents.split(copyMatch)[1].trimStart();
+    await fs.writeFile(filePath, newContents, 'utf-8');
+  }
+};
+
+await Promise.all(files.map(deleteCopyrights));
